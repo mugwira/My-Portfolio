d451c7b405e5badfe8bfcb38532062a43256ad96
Mugwira <mugwiram@gmail.com>
2021-11-30 16:06:27 +0200

    Added links to my socials buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,23 +5,41 @@ import { FaFacebookF, FaGithub } from "react-icons/fa";
 function Contact() {
   return (
     <div className="bg-gray-300 p-8">
+      <h1 className="font-mono text-4xl fonr-semi-bold text-center p-16">
+        Connect with me
+      </h1>
       <div className="container contact-details flex justify-center items-center">
-        <div className="flex-1 justify-center socials text-4xl space-y-4 text-blue-800">
-          <AiOutlineTwitter />
-          <AiFillLinkedin />
-          <FaFacebookF />
-          <FaGithub />
+        <div className=" flex flex-1 justify-center socials text-4xl gap-8 text-blue-600">
+          <a className="hover:text-blue-500" href="https://twitter.com/home" target="_blank" rel="noreferrer"><AiOutlineTwitter /></a>
+          <a className="hover:text-blue-500" href="https://linkedin.com/home" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
+          <a className="hover:text-blue-500" href="https://facebook.com/home" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+          <a className="hover:text-blue-500" href="https://github.com/home" target="_blank" rel="noreferrer"><FaGithub /></a>
+          
+          
+          
         </div>
         <div className="contacts w-full bg-gray-900 flex-1">
-          <form className="flex flex-col px-8  py-16 px-2 space-y-8">
+          <form className="flex flex-col px-8  py-8 px-2 space-y-8">
             <label className="text-gray-200">Name</label>
-            <input  className="py-1.5 px-2"type="text" placeholder="Enter full name" />
+            <input
+              className="py-1.5 px-2"
+              type="text"
+              placeholder="Enter full name"
+            />
             <label className="text-gray-200">Email Address</label>
-            <input  className="py-1.5 px-2"type="email" placeholder="Enter email address" />
+            <input
+              className="py-1.5 px-2"
+              type="email"
+              placeholder="Enter email address"
+            />
             <label className="text-gray-200">Message</label>
-            <textarea className="py-4 px-2" placeholder="Type your message">
-            </textarea>
-            <button className="px-4 py-2 bg-gray-600 rounded-lg text-gray-200">Send</button>
+            <textarea
+              className="py-4 px-2"
+              placeholder="Type your message"
+            ></textarea>
+            <button className="px-4 py-2 bg-gray-600 rounded-lg text-gray-200">
+              Send
+            </button>
           </form>
         </div>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,53 +1,54 @@
 import React from "react";
-import {IoLogoJavascript, IoLogoHtml5, IoLogoCss3} from 'react-icons/io'
-import {SiJquery} from 'react-icons/si'
-import {FaReact} from 'react-icons/fa'
-import {SiTailwindcss} from 'react-icons/si'
-import {FaBootstrap} from 'react-icons/fa'
-import {AiFillGithub} from 'react-icons/ai'
-
+import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
+import { SiJquery } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 
 function Skills() {
   return (
     <div className="bg-gray-100 h-screen">
-      <h1 className="font-mono text-4xl fonr-semi-bold text-center pt-16">Skill Set</h1>
+      <h1 className="font-mono text-4xl fonr-semi-bold text-center pt-32">
+        Skill Set
+      </h1>
       <div className="container flex justify-between gap-4 p-16 ">
-        <div className="left space-y-8 flex-1 ">
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <IoLogoJavascript />
-            <h1>JavaScript</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <FaReact />
-            <h1>React Js</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <IoLogoHtml5 />
-            <h1>HTML</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <IoLogoCss3 />
-            <h1>CSS</h1>
-          </div>
+        {/* <div className="left space-y-8 flex-1 "> */}
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <IoLogoJavascript />
+          <h1 className="text-lg">JavaScript</h1>
         </div>
-        <div className="right space-y-8 flex-1  ">
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <SiJquery />
-            <h1>JQuery</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <SiTailwindcss />
-            <h1>Tailwind CSS</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <FaBootstrap />
-            <h1>Bootstrap</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2 text-2xl">
-            <AiFillGithub />
-            <h1>Git/GitHub</h1>
-          </div>
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <FaReact />
+          <h1 className="text-lg">React Js</h1>
         </div>
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <IoLogoHtml5 />
+          <h1 className="text-lg">HTML</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <IoLogoCss3 />
+          <h1 className="text-lg">CSS</h1>
+        </div>
+        {/* </div> */}
+        {/* <div className="right space-y-8 flex-1  "> */}
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <SiJquery />
+          <h1 className="text-lg">JQuery</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <SiTailwindcss />
+          <h1 className="text-lg">Tailwind CSS</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <FaBootstrap />
+          <h1 className="text-lg">Bootstrap</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-2 text-2xl bg-gray-50 rounded-lg shadow-2xl w-1/3 py-1">
+          <AiFillGithub />
+          <h1 className="text-lg">Git/GitHub</h1>
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
